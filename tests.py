a5224427a26afd8a6fb1e9762510b04999b6ad0c
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-04-14 22:01:03 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 
+import pytest
+
 from main import app
 
 client = TestClient(app)
@@ -9,10 +11,24 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-    
-    
-def test_hello_name():
-    name = 'Kamila'
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
     response = client.get(f"/hello/{name}")
     assert response.status_code == 200
-    assert response.text == f'"Hello {name}"'
+    assert response.json() == {"msg": f"Hello {name}"}
+
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
+    # 3rd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "3"
