2a57e5e83a6e8015c7d510a722b9fb2cb77e7061
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-04-14 21:58:15 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,3 +9,10 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+    
+    
+def test_hello_name():
+    name = 'Kamila'
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
