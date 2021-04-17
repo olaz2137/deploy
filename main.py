7a5f65ba7e6e51f7f035cf3030fe88129abdfa33
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-04-17 15:48:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,25 @@ def counter():
 def hello_name_view(name: str):
     return HelloResp(msg=f"Hello {name}")
 
-#@app.get("/method")
-#def method():
+@app.get("/method")
+def get():
+    return {"method": "GET"}
+
+@app.post("/method")
+def post():
+    return {"method": "POST"}
+
+@app.delete("/method")
+def delete():
+    return {"method": "DELETE"}
+
+@app.put("/method")
+def put():
+    return {"method": "PUT"}
+
+@app.get("/method")
+def options():
+    return {"method": "OPTIONS"}
+
+
     
