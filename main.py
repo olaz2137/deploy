49b2de99baf928061ef57b9279feb2c9f02296e5
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-04-18 19:05:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def put():
 def options():
     return {"method": "OPTIONS"}
 
-@app.get("/auth",status_code=204)
+@app.get("/auth")
 def auth(password: str, password_hash: str):
     if password_hash != hashlib.sha512(password):
          return Response(status=status.HTTP_401_UNAUTHORIZED)
