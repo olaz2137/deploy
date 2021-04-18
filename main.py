aaa61da203c2edea7aabddf841219fd6332bbaa9
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-04-18 20:55:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ def options():
 
 @app.get("/auth")
 def auth(password: str, password_hash: str):
-#   if password_hash != hashlib.sha512(password):
-#       return Response(status_code=status.HTTP_401_UNAUTHORIZED)
+   if password_hash != hashlib.sha512(password).hexdigest():
+       return Response(status_code=status.HTTP_401_UNAUTHORIZED)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 
