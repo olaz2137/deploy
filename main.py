1938078159710daf8bd87d26b8145f0efb2af744
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-04 09:18:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ def welcome_token(*,response: Response, token: str = Query(None), format: str = 
         return PlainTextResponse("Welcome!")
     
 @app.delete("/logout_session/")
-async def logout_session(*, response: Response, session_token: str = Cookie(None), format: str = Query(None)):
+async def logout_session(session_token: str = Cookie(None), format: str = Query(None)):
     if session_token != app.session_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -204,7 +204,7 @@ async def logout_session(*, response: Response, session_token: str = Cookie(None
     return response
 
 @app.delete("/logout_token/")
-async def logout_token(*,response: Response, token: str = Query(None), format: str = Query(None)):
+async def logout_token(token: str = Query(None), format: str = Query(None)):
     if token != app.token_value:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -215,7 +215,7 @@ async def logout_token(*,response: Response, token: str = Query(None), format: s
     return RedirectResponse(url="/logged_out?format={format}", status_code=303)
 
 @app.get("/logged_out/")
-def logged_out(*,response: Response, format: str = Query(None)):
+def logged_out(format: str = Query(None)):
     if format == "json":
         return {"message": "Logged out!"}
     elif format == "html":
