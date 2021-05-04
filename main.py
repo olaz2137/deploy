71dc7226ab69f6648daa96f46d2975757bec1fa0
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-04 17:40:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def welcome_token(*,response: Response, token: str = Query(None), format: str = 
  
 
 @app.delete("/logout_session/")
-async def logout_session(*, response: Response, session_token: str = Cookie(None), format: str = Query(None)):
+async def logout_session(*, response: Response, session_token: str = Cookie(None), format: str = Query("")):
     if session_token != app.session_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -201,7 +201,7 @@ async def logout_session(*, response: Response, session_token: str = Cookie(None
     return RedirectResponse(url=f"/logged_out?format={format}",status_code=303)
 
 @app.delete("/logout_token/")
-async def logout_token(*,response: Response, token: str = Query(None), format: str = Query(None)):
+async def logout_token(*,response: Response, token: str = Query(None), format: str = Query("")):
     if token != app.token_value:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
